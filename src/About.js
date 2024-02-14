fc723da15ce2deb7c8c20506325e7ce604ff575f
Vlad Popescu <popescu-9@student.tudelft.nl>
2024-02-14 20:13:53 +0200

    responsive and about page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./CardContainer.css";
+import "./About.css";
+import emailIcon from "./images/emailIcon.png";
+import githubIcon from "./images/githubIcon.png";
+import linkedinIcon from "./images/linkedinIcon.png";
+import mePhoto from "./images/me.jpg";
 
 function About() {
   return (
-    <div className="app">
-      <div className="app-content">
-      
+    <div
+      className="body-container"
+      style={{ background: "#BEE3DB", marginTop: 30, color: "#555B6E" }}
+    >
+      <div className="bio">
+        <img src={mePhoto} alt="personal" className="me-photo" />
+        <div>
+          <h2>About Me</h2>
+          <p>
+            Hi there! My name is Vlad Popescu and I'm a student at Delft
+            University of Technology. I love coding and building things that
+            make a difference. Feel free to reach out to me if you have any
+            questions or just want to connect!
+          </p>
+        </div>
+      </div>
+      <div className="contact-info">
+        <h2>Contact Information</h2>
+        <ul>
+          <li className="info-row">
+            <img src={emailIcon} alt="Email" className="link-icon" />
+            Email: vl_popescu@protonmail.com
+          </li>
+          <li className="info-row">
+            <img src={githubIcon} alt="Github" className="link-icon" />
+
+            <a
+              href="https://github.com/vladpopescu0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub: github.com/vladpopescu0
+            </a>
+          </li>
+          <li className="info-row">
+            <img src={linkedinIcon} alt="LinkedIn" className="link-icon" />
+
+            <a
+              href="https://www.linkedin.com/in/vlad-stefan-popescu-8a521b1a1/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn: linkedin.com/in/vlad-stefan-popescu-8a521b1a1/
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,49 +1,69 @@
-import React , {useState, useEffect} from 'react';
-import './Card.css';
-import calendarIcon from './images/calendar.png'; 
+import React, { useState, useEffect } from "react";
+import "./Card.css";
+import calendarIcon from "./images/calendar.png";
 const Card = (props) => {
-    const { title, content, imageUrl, dateAdded } = props;
-    const [isTextVisible, setIsTextVisible] = useState(true);
+  const { title, content, imageUrl, dateAdded } = props;
+  const [isTextVisible, setIsTextVisible] = useState(true);
+  const [fontSize, setFontSize] = useState(18);
+  const [imageSize, setImageSize] = useState(200);
 
-    const checkWindowSize = () => {
-        if (window.innerWidth < 900) {
-          setIsTextVisible(false);
-        } else {
-          setIsTextVisible(true);
-        }
+  const checkWindowSize = () => {
+    if (window.innerWidth < 600) {
+      setImageSize(130);
+      setFontSize(7);
+    } else if (window.innerWidth < 900 && window.innerWidth >= 600) {
+      setFontSize(12);
+      setIsTextVisible(true);
+      setImageSize(180);
+    } else {
+      setFontSize(18);
+      setIsTextVisible(true);
+      setImageSize(200);
+    }
+  };
+
+  useEffect(() => {
+    checkWindowSize();
+    window.addEventListener("resize", checkWindowSize);
+
+    return () => {
+      window.removeEventListener("resize", checkWindowSize);
     };
+  });
 
-    useEffect(()=>{
-        checkWindowSize();
-        window.addEventListener('resize',checkWindowSize);
+  return (
+    <div className="card">
+      {/* Render the image if an imageUrl is provided */}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={title}
+          className="card-image"
+          style={{ width: imageSize, height: "100%" }}
+        />
+      )}
 
-        return () =>{
-            window.removeEventListener('resize',checkWindowSize);
-        }
+      <div className="card-text">
+        {/* Render the title */}
+        <h2 className="card-title" style={{ fontSize: fontSize * 2 }}>
+          {title}
+        </h2>
 
-    });
+        {/* Render the content */}
+        {isTextVisible && (
+          <p className="card-content" style={{ fontSize: fontSize }}>
+            {content}
+          </p>
+        )}
 
-
-    return (
-        <div className="card">
-            {/* Render the image if an imageUrl is provided */}
-            {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
-
-            <div className="card-text">
-                {/* Render the title */}
-                <h2 className="card-title">{title}</h2>
-
-                {/* Render the content */}
-                {isTextVisible && <p className="card-content">{content}</p>}
-
-                {/* Render the date added with calendar icon */}
-                <div className="date-added">
-                    <img src={calendarIcon} alt="Calendar" className="calendar-icon" />
-                    <span className="date-text">{dateAdded}</span>
-                </div>
-            </div>
+        {/* Render the date added with calendar icon */}
+        <div className="date-added">
+          <img src={calendarIcon} alt="Calendar" className="calendar-icon" />
+          <span className="date-text">{dateAdded}</span>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
